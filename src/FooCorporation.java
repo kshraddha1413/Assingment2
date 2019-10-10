788f6382be6c5cabde1b92a2a74e991081bd913a
Travis Lewis <travis.of.lewis@gmail.com>
2019-10-09 21:30:07 -0400

    updated

--- src/FooCorporation.java
+++ src/FooCorporation.java
@@ -2,30 +2,31 @@
 public class FooCorporation {
 
 	public static void main(String[] args) {
-		double EmpPay1=totalPay(7.50,35);
-		System.out.println("Employee1 payment:"+EmpPay1);
-		double EmpPay2=totalPay(8.20,47);
-		System.out.println("Employee2 payment:"+EmpPay2);
-		double EmpPay3=totalPay(10.00,73);
-		System.out.println("Employee3 payment:"+EmpPay3);		
-	}  
-	
-	public static double totalPay(double basePay,double hoursWorked){
- 	   double totalPayment=0;
- 	   if(basePay<8){
- 		  System.err.println("BasePay is lessthen $8.00 an hour");
- 	   }
- 	   if (hoursWorked>60) {
- 		   System.err.println("number of hours greaterthen 60");
- 	   }
- 	  if(hoursWorked<=40) {
- 		   totalPayment=basePay*hoursWorked;
- 		   return totalPayment;
- 	 }else{
- 		   double extraHours=hoursWorked-40;
- 		   totalPayment=(basePay*hoursWorked)+extraHours*basePay*1.5;
- 		   return totalPayment;
- 	 }
- 	  
-}
+		double Emp1 = totalPay(7.50, 35);
+		System.out.println("base pay : 7.50 hours worked: 35" + "Employee1 payment:" + Emp1);
+		double Emp2 = totalPay(8.20, 47);
+		System.out.println("base pay : 8.20 hours worked: 47" + "Employee2 payment:" + Emp2);
+		double Emp3 = totalPay(10.00, 73);
+		System.out.println("base pay : 10.00 hours worked: 73" + "Employee3 payment:" + Emp3);
+	}
+
+	public static double totalPay(double basePay, double hoursWorked) {
+		double totalPayment = 0;
+
+		if ((basePay < 8.00) || (hoursWorked > 60)) {
+			System.err.println("base pay :" + basePay + "hours worked: " + hoursWorked
+					+ " Either BasePay is less than $8.00 or hours are more than 60");
+		} else {
+			if (hoursWorked <= 40) {
+				totalPayment = basePay * hoursWorked;
+				
+			} else {
+				double extraHours = hoursWorked - 40;
+				totalPayment = (basePay * hoursWorked) + extraHours * basePay * 1.5;
+				
+			}
+		}return totalPayment;
+
+	}
+
 }
